2063508c4e8e5e4983142133de4118ae21007c21
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-07-15 19:20:54 +0300

    add icon for refresh button

--- src/components/RefreshButton/RefreshButton.tsx
+++ src/components/RefreshButton/RefreshButton.tsx
@@ -1,4 +1,5 @@
 import s from './RefreshButton.module.css';
+import { FiRefreshCw } from 'react-icons/fi';
 
 type Props = {
   disabled: boolean;
@@ -7,6 +8,7 @@ type Props = {
 
 export const RefreshButton = ({ disabled, onClick }: Props) => (
   <button className={s.refreshButton} onClick={onClick} disabled={disabled}>
+    <FiRefreshCw className={s.icon} />
     Refresh
   </button>
 );
