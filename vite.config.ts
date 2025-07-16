6c9da6b69b054fbb0c7cd87ffa38dc52375528cf
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-07-16 22:43:14 +0300

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/custom-super-data-picker/",
   plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        additionalData: `@import "./src/styles/variables.css";`,
-      },
-    },
-  },
 });
