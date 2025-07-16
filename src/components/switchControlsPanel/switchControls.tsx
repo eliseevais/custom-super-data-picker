edcd6e51de4d727262ac1820a1633bb2a2468e52
Irina Eliseeva <irina.eliseeva1991@gmail.com>
2025-07-16 22:16:48 +0300

    refactor

--- src/components/switchControlsPanel/switchControls.tsx
+++ src/components/switchControlsPanel/switchControls.tsx
@@ -8,68 +8,76 @@ import {
 import type { WidthMode } from "../../types/types.ts";
 
 export type Props = {
-  showCommonlyUsed: boolean;
-  setShowCommonlyUsed: (value: boolean) => void;
-  showRecentlyUsed: boolean;
-  setShowRecentlyUsed: (value: boolean) => void;
-  showManualInput: boolean;
-  setShowManualInput: (value: boolean) => void;
-  showCustomContent: boolean;
-  setShowCustomContent: (value: boolean) => void;
+  timeSettings: {
+    showCommonlyUsed: boolean;
+    setShowCommonlyUsed: (value: boolean) => void;
+    showRecentlyUsed: boolean;
+    setShowRecentlyUsed: (value: boolean) => void;
+    showManualInput: boolean;
+    setShowManualInput: (value: boolean) => void;
+    showCustomContent: boolean;
+    setShowCustomContent: (value: boolean) => void;
+  };
 
-  showRefreshButton: boolean;
-  setShowRefreshButton: (value: boolean) => void;
-  refreshFilled: boolean;
-  setRefreshFilled: (value: boolean) => void;
-  refreshIconOnly: boolean;
-  setRefreshIconOnly: (value: boolean) => void;
+  refreshSettings: {
+    showRefreshButton: boolean;
+    setShowRefreshButton: (value: boolean) => void;
+    refreshFilled: boolean;
+    setRefreshFilled: (value: boolean) => void;
+    refreshIconOnly: boolean;
+    setRefreshIconOnly: (value: boolean) => void;
+  };
 
-  compactInputs: boolean;
-  setCompactInputs: (value: boolean) => void;
-  inputLineWidthMode: WidthMode;
-  setInputLineWidthMode: (value: WidthMode) => void;
+  displaySettings: {
+    compactInputs: boolean;
+    setCompactInputs: (value: boolean) => void;
+    inputLineWidthMode: WidthMode;
+    setInputLineWidthMode: (value: WidthMode) => void;
+  };
 
-  isAutoRefreshOn: boolean;
-  setIsAutoRefreshOn: (value: boolean) => void;
-  refreshInterval: number;
-  setRefreshInterval: (value: number) => void;
+  autoRefreshSettings: {
+    isAutoRefreshOn: boolean;
+    setIsAutoRefreshOn: (value: boolean) => void;
+    refreshInterval: number;
+    setRefreshInterval: (value: number) => void;
+  };
 };
 
 export const SwitchControls = (props: Props) => {
   return (
     <div className={s.switchControls}>
       <TimeSettings
-        showCommonlyUsed={props.showCommonlyUsed}
-        showRecentlyUsed={props.showRecentlyUsed}
-        showManualInput={props.showManualInput}
-        showCustomContent={props.showCustomContent}
-        setShowCommonlyUsed={props.setShowCommonlyUsed}
-        setShowRecentlyUsed={props.setShowRecentlyUsed}
-        setShowManualInput={props.setShowManualInput}
-        setShowCustomContent={props.setShowCustomContent}
+        showCommonlyUsed={props.timeSettings.showCommonlyUsed}
+        setShowCommonlyUsed={props.timeSettings.setShowCommonlyUsed}
+        showRecentlyUsed={props.timeSettings.showRecentlyUsed}
+        setShowRecentlyUsed={props.timeSettings.setShowRecentlyUsed}
+        showManualInput={props.timeSettings.showManualInput}
+        setShowManualInput={props.timeSettings.setShowManualInput}
+        showCustomContent={props.timeSettings.showCustomContent}
+        setShowCustomContent={props.timeSettings.setShowCustomContent}
       />
 
       <RefreshButtonSettings
-        showRefreshButton={props.showRefreshButton}
-        refreshFilled={props.refreshFilled}
-        refreshIconOnly={props.refreshIconOnly}
-        setShowRefreshButton={props.setShowRefreshButton}
-        setRefreshFilled={props.setRefreshFilled}
-        setRefreshIconOnly={props.setRefreshIconOnly}
+        showRefreshButton={props.refreshSettings.showRefreshButton}
+        setShowRefreshButton={props.refreshSettings.setShowRefreshButton}
+        refreshFilled={props.refreshSettings.refreshFilled}
+        setRefreshFilled={props.refreshSettings.setRefreshFilled}
+        refreshIconOnly={props.refreshSettings.refreshIconOnly}
+        setRefreshIconOnly={props.refreshSettings.setRefreshIconOnly}
       />
 
       <DisplaySettings
-        compactInputs={props.compactInputs}
-        setCompactInputs={props.setCompactInputs}
-        inputLineWidthMode={props.inputLineWidthMode}
-        setInputLineWidthMode={props.setInputLineWidthMode}
+        compactInputs={props.displaySettings.compactInputs}
+        setCompactInputs={props.displaySettings.setCompactInputs}
+        inputLineWidthMode={props.displaySettings.inputLineWidthMode}
+        setInputLineWidthMode={props.displaySettings.setInputLineWidthMode}
       />
 
       <AutoRefreshToggle
-        isAutoRefreshOn={props.isAutoRefreshOn}
-        setIsAutoRefreshOn={props.setIsAutoRefreshOn}
-        refreshInterval={props.refreshInterval}
-        setRefreshInterval={props.setRefreshInterval}
+        isAutoRefreshOn={props.autoRefreshSettings.isAutoRefreshOn}
+        setIsAutoRefreshOn={props.autoRefreshSettings.setIsAutoRefreshOn}
+        refreshInterval={props.autoRefreshSettings.refreshInterval}
+        setRefreshInterval={props.autoRefreshSettings.setRefreshInterval}
       />
     </div>
   );
